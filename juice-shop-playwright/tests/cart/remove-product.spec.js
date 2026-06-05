899318d13659f23d978450bd5eedefea963be66b
Alejandrrooo <alejandroreyes.dig@gmail.com>
2026-06-04 20:53:05 -0600

    test(cart): add automated remove product from cart

---
+++ juice-shop-playwright/tests/cart/remove-product.spec.js
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+import { goToRegister } from '../helpers/goToRegister';
+import { generateUser } from '../utils/dataGenerator';
+import { doRegistration} from '../helpers/doRegister';
+import { doLogin} from '../helpers/login';
+
+test ('removeProductFromCart', async ({page}) =>{
+    const user = generateUser()
+
+    await goToRegister(page)
+    await doRegistration(page,user)
+    await doLogin(page,user)
+
+    const appleJuiceCard = page
+        .locator('mat-card')
+        .filter({ has: page.locator('[alt= "Apple Juice (1000ml)"]')})
+
+    await appleJuiceCard.getByRole('button', { name: 'Add to Basket'}).click();
+
+    const basketCount = page.locator('.fa-layers-counter')
+    await basketCount.click()
+
+    const appleJuiceRow = page
+        .locator('mat-row')
+        .filter({hasText: 'Apple Juice (1000ml)'})
+
+    const deleteButton = appleJuiceRow
+        .locator('.mat-column-remove button')
+
+    await deleteButton.click()
+
+    await expect(appleJuiceRow).not.toBeVisible()
+    await expect(page.locator('mat-row')).toHaveCount(0)
+
+
+
+
+
+    
+
+
+
+
+
+    //const deleteProduct = page.locator('#svg-inline--fa fa-trash-alt fa-w-14')
+
+})
