ea1ab32f36bb57378a580f7b5d46e22a9d835a82
Alejandrrooo <alejandroreyes.dig@gmail.com>
2026-08-27 20:27:18 -0600

    test(checkout): rewrite addAddress spec with proper test title

--- juice-shop-playwright/tests/checkout/addAddress.spec.js
+++ juice-shop-playwright/tests/checkout/addAddress.spec.js
@@ -3,7 +3,7 @@ import { goToRegister } from '../helpers/goToRegister';
 import { generateUser } from '../utils/dataGenerator';
 import { doRegistration } from '../helpers/doRegister';
 import { doLogin } from '../helpers/login';
-import { startCheckout } from '../helpers/startCheckout';
+import { goToAddAddress } from '../helpers/goToAddAddress';
 
 test('addNewAddress', async ({ page }) => {
     const user = generateUser();
@@ -11,10 +11,32 @@ test('addNewAddress', async ({ page }) => {
     await goToRegister(page);
     await doRegistration(page, user);
     await doLogin(page, user);
-    await startCheckout(page); 
-     
-    const new_address_button = page.locator('button', { hasText: 'Add New Address' })
-    await expect(new_address_button).toBeVisible({ timeout: 10000 })
-    await new_address_button.click()
-   
+    await goToAddAddress(page);
+
+    const countryInput = page.getByPlaceholder('Please provide a country.')
+    await countryInput.fill('United States')
+
+    const userNameInput = page.getByPlaceholder('Please provide a name')
+    await userNameInput.fill('Test User')
+
+    const phoneInput = page.getByPlaceholder('Please provide a mobile number.')
+    await phoneInput.fill('1234567890')
+
+    const zipCodeInput = page.getByPlaceholder('Please provide a ZIP code.')
+    await zipCodeInput.fill('12345')
+
+    const addressInput = page.getByPlaceholder('Please provide an address.')
+    await addressInput.fill('123 Test St')
+
+    const cityInput = page.getByPlaceholder('Please provide a city.')
+    await cityInput.fill('Test City')
+
+    const stateInput = page.getByPlaceholder('Please provide a state.')
+    await stateInput.fill('Test State')
+
+    await page.locator('#submitButton').click()
+
+    await expect(page.getByText('Test User')).toBeVisible({ timeout: 5000 })
+
+
 })
